e7a22d7456e9e70b22aff515859c8ee79798151d
Lucas Saaler <lucassaaler00@gmail.com>
2022-06-15 19:50:57 -0400

    added basic functionality to food generation

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
-var generateButtonEl = document.querySelector('#generateButton')
+var generateButtonEl = document.querySelector('#generate')
+var mealEl = document.querySelector('#meal')
+var cultureEl = document.querySelector('#culture')
+var categoryEl = document.querySelector('#category')
+var recipeEl = document.querySelector('#recipe')
+var foodImageEl = document.querySelector('#foodImage')
 
 function getFood(){
     var queryURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
@@ -9,6 +14,12 @@ function getFood(){
     })
     .then(function(data){
         console.log(data);
+        mealEl.textContent = data.meals[0].strMeal
+        cultureEl.textContent = data.meals[0].strArea
+        categoryEl.textContent = data.meals[0].strCategory
+        recipeEl.textContent = "Link to recipe"
+        recipeEl.setAttribute("href", data.meals[0].strSource)
+        foodImageEl.setAttribute("src", data.meals[0].strMealThumb)
     });
 }
 
@@ -24,5 +35,5 @@ function getDrink(){
     });
 }
 
-getFood()
-getDrink()
+generateButtonEl.addEventListener("click", getFood)
+generateButtonEl.addEventListener("click", getDrink)
